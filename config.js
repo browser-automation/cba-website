c60f9990fe73510d5c759b1bcd1429796027f8f1
naarrek <h.narek@gmail.com>
2020-04-11 12:22:28 +0400

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const templateData =
 {
   site: {
+<<<<<<< HEAD
     title: "CMintS Starter", // See "layouts/_head.ejs"
     navigation: [
       {
@@ -14,9 +15,445 @@ const templateData =
       {
         title: "Github",
         link: "https://github.com/cmints/single-lang-starter"
+=======
+    title: "CBA", // See "layouts/_head.ejs"
+    navigation: [
+      {
+        title: "Home",
+        link: "#",
+        sub_items:[]
+      },
+
+      {
+        title:"Documentation",
+        link: "#",
+        sub_items:[
+                    {
+                  title: "Interface",
+                  link:"",
+                  sub_items:[
+                            {
+                              title: "Project Datagrid",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "Actions Datagrid",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                                title: "Functions Datagrid",
+                                link:"",
+                                sub_items:[]
+                                }
+                            ]
+                },
+                  {
+                  title:"Actions",
+                  link:"",
+                  sub_items:[
+                            {
+                              title: "inject",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "inject-cs",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "bg-inject",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "bg-function (deprecated)",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "change",
+                              link:"",
+                              sub_items:[]
+                            },
+                            {
+                              title: "check",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "click",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "submit-click",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "update",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "timer",
+                              link:"",
+                              sub_items:[]
+                            },
+                            {
+                              title: "redirect",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "copy",
+                              link:"",
+                              sub_items:[]
+                              },
+                              {
+                              title: "pause",
+                              link:"",
+                              sub_items:[]
+                            },
+
+                            ]
+                },
+                {
+                  title:"Functions",
+                  link:"",
+                  sub_items:[
+                            {
+                              title: "clear cookies",
+                              link:"",
+                              sub_items:[]
+                            },
+                          ]
+                },
+                {
+                  title:"Recording",
+                  link:"",
+                  sub_items:[]
+                },
+                {
+                  title:"Play",
+                  link:"",
+                  sub_items:[]
+                },
+                {
+                  title:"Clipboard",
+                  link:"",
+                  sub_items:[]
+                },
+                {
+                  title:"Options",
+                  link:"",
+                  sub_items:[
+                              {
+                                title: "CBA",
+                                link:"",
+                                sub_items:[]
+                              },
+                              {
+                                title: "Functions Option",
+                                link:"",
+                                sub_items:[]
+                              },
+                              {
+                                title: "Import-Export",
+                                link:"",
+                                sub_items:[]
+                              },
+
+                  ]
+                }
+              ]
+      },
+        sub_items: []
+      },
+
+      {
+        title: "Documentation",
+        link: "#",
+        sub_items: [
+          {
+            title: "Interface",
+            link: "",
+            sub_items: [
+              {
+                title: "Project Datagrid",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "Actions Datagrid",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "Functions Datagrid",
+                link: "",
+                sub_items: []
+              }
+            ]
+          },
+          {
+            title: "Actions",
+            link: "",
+            sub_items: [
+              {
+                title: "inject",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "inject-cs",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "bg-inject",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "bg-function (deprecated)",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "change",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "check",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "click",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "submit-click",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "update",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "timer",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "redirect",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "copy",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "pause",
+                link: "",
+                sub_items: []
+              },
+
+            ]
+          },
+          {
+            title: "Functions",
+            link: "",
+            sub_items: [
+              {
+                title: "clear cookies",
+                link: "",
+                sub_items: []
+              },
+            ]
+          },
+          {
+            title: "Recording",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Play",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Clipboard",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Options",
+            link: "",
+            sub_items: [
+              {
+                title: "CBA",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "Functions Option",
+                link: "",
+                sub_items: []
+              },
+              {
+                title: "Import-Export",
+                link: "",
+                sub_items: []
+              },
+
+            ]
+          }
+        ]
+      },
+
+      {
+        title: "Tutorial",
+        link: "#",
+        sub_items: [
+                    {
+                      title: "Registration Demo",
+                      link:"",
+                      sub_items:[]
+                    },
+                    {
+                      title: "Facebook Demo",
+                      link:"",
+                      sub_items:[]
+                    },
+                    {
+                      title: "Recording",
+                      link:"",
+                      sub_items:[]
+                    },
+                    {
+                      title: "Video Presentation for CBA",
+                      link:"",
+                      sub_items:[]
+                    }
+        ]
+      },
+      {
+        title: "Ready Projects",
+        link:"",
+        sub_items:[
+                  {
+                    title: "FB like/unlike",
+                    link:"",
+                    sub_items:[]
+                  }
+                ]
+
+          {
+            title: "Registration Demo",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Facebook Demo",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Recording",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Video Presentation for CBA",
+            link: "",
+            sub_items: []
+          }
+        ]
+
+      },
+
+      {
+
+        title: "What's New",
+        link:"",
+        sub_items:[
+                  {
+                    title: "Version 8.0",
+                    link:"",
+                    sub_items:[]
+                  },
+                  {
+                    title: "Version 6.0",
+                    link:"",
+                    sub_items:[]
+                  },
+                  {
+                    title: "Version 5.0",
+                    link:"",
+                    sub_items:[]
+                  }
+        ]
+      }
+
+        title: "Ready Projects",
+        link: "",
+        sub_items: [
+          {
+            title: "FB like/unlike",
+            link: "",
+            sub_items: []
+          }
+        ]
+      },
+
+      {
+        title: "What's New",
+        link: "",
+        sub_items: [
+          {
+            title: "Version 8.0",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Version 6.0",
+            link: "",
+            sub_items: []
+          },
+          {
+            title: "Version 5.0",
+            link: "",
+            sub_items: []
+          }
+        ]
+>>>>>>> config.js
       }
     ]
   }
 };
 
+<<<<<<< HEAD
 module.exports = {templateData};
+=======
+
+
+
+  const port = {
+  http: 3000};
+module.exports = {templateData,port};
+const port = {
+  http: 3000
+};
+module.exports = { templateData, port };
+>>>>>>> config.js
