89ac94928ef23de0d69d5b08c13c5d9435617cc4
naarrek <h.narek@gmail.com>
2020-04-12 01:43:42 +0400

    3-rd version of header tested with linters

--- config.js
+++ config.js
@@ -1,163 +1,12 @@
 const templateData =
 {
-
   site: {
-    title: "CBA", // See "layouts/_head.ejs"
+    title: "CBA",
+    subtitle: "Chromium Browser Automation (extension for chrome browser automation)",
     navigation: [
       {
         title: "Home",
-        link: "#",
-        sub_items:[]
-      },
-
-      {
-        title:"Documentation",
-        link: "#",
-        sub_items:[
-                    {
-                  title: "Interface",
-                  link:"",
-                  sub_items:[
-                            {
-                              title: "Project Datagrid",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "Actions Datagrid",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                                title: "Functions Datagrid",
-                                link:"",
-                                sub_items:[]
-                                }
-                            ]
-                },
-                  {
-                  title:"Actions",
-                  link:"",
-                  sub_items:[
-                            {
-                              title: "inject",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "inject-cs",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "bg-inject",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "bg-function (deprecated)",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "change",
-                              link:"",
-                              sub_items:[]
-                            },
-                            {
-                              title: "check",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "click",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "submit-click",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "update",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "timer",
-                              link:"",
-                              sub_items:[]
-                            },
-                            {
-                              title: "redirect",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "copy",
-                              link:"",
-                              sub_items:[]
-                              },
-                              {
-                              title: "pause",
-                              link:"",
-                              sub_items:[]
-                            },
-
-                            ]
-                },
-                {
-                  title:"Functions",
-                  link:"",
-                  sub_items:[
-                            {
-                              title: "clear cookies",
-                              link:"",
-                              sub_items:[]
-                            },
-                          ]
-                },
-                {
-                  title:"Recording",
-                  link:"",
-                  sub_items:[]
-                },
-                {
-                  title:"Play",
-                  link:"",
-                  sub_items:[]
-                },
-                {
-                  title:"Clipboard",
-                  link:"",
-                  sub_items:[]
-                },
-                {
-                  title:"Options",
-                  link:"",
-                  sub_items:[
-                              {
-                                title: "CBA",
-                                link:"",
-                                sub_items:[]
-                              },
-                              {
-                                title: "Functions Option",
-                                link:"",
-                                sub_items:[]
-                              },
-                              {
-                                title: "Import-Export",
-                                link:"",
-                                sub_items:[]
-                              },
-
-                  ]
-                }
-              ]
-      },
-        sub_items: []
+        link: "#"
       },
 
       {
@@ -170,18 +19,15 @@ const templateData =
             sub_items: [
               {
                 title: "Project Datagrid",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "Actions Datagrid",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "Functions Datagrid",
-                link: "",
-                sub_items: []
+                link: ""
               }
             ]
           },
@@ -191,68 +37,55 @@ const templateData =
             sub_items: [
               {
                 title: "inject",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "inject-cs",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "bg-inject",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "bg-function (deprecated)",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "change",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "check",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "click",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "submit-click",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "update",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "timer",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "redirect",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "copy",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "pause",
-                link: "",
-                sub_items: []
+                link: ""
               },
 
             ]
@@ -263,25 +96,21 @@ const templateData =
             sub_items: [
               {
                 title: "clear cookies",
-                link: "",
-                sub_items: []
+                link: ""
               },
             ]
           },
           {
             title: "Recording",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Play",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Clipboard",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Options",
@@ -289,18 +118,140 @@ const templateData =
             sub_items: [
               {
                 title: "CBA",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "Functions Option",
-                link: "",
-                sub_items: []
+                link: ""
               },
               {
                 title: "Import-Export",
-                link: "",
-                sub_items: []
+                link: ""
+              },
+
+            ]
+          }
+        ]
+      },
+
+      {
+        title: "Documentation",
+        link: "#",
+        sub_items: [
+          {
+            title: "Interface",
+            link: "",
+            sub_items: [
+              {
+                title: "Project Datagrid",
+                link: ""
+              },
+              {
+                title: "Actions Datagrid",
+                link: ""
+              },
+              {
+                title: "Functions Datagrid",
+                link: ""
+              }
+            ]
+          },
+          {
+            title: "Actions",
+            link: "",
+            sub_items: [
+              {
+                title: "inject",
+                link: ""
+              },
+              {
+                title: "inject-cs",
+                link: ""
+              },
+              {
+                title: "bg-inject",
+                link: ""
+              },
+              {
+                title: "bg-function (deprecated)",
+                link: ""
+              },
+              {
+                title: "change",
+                link: ""
+              },
+              {
+                title: "check",
+                link: ""
+              },
+              {
+                title: "click",
+                link: ""
+              },
+              {
+                title: "submit-click",
+                link: ""
+              },
+              {
+                title: "update",
+                link: ""
+              },
+              {
+                title: "timer",
+                link: ""
+              },
+              {
+                title: "redirect",
+                link: ""
+              },
+              {
+                title: "copy",
+                link: ""
+              },
+              {
+                title: "pause",
+                link: ""
+              },
+
+            ]
+          },
+          {
+            title: "Functions",
+            link: "",
+            sub_items: [
+              {
+                title: "clear cookies",
+                link: ""
+              },
+            ]
+          },
+          {
+            title: "Recording",
+            link: ""
+          },
+          {
+            title: "Play",
+            link: ""
+          },
+          {
+            title: "Clipboard",
+            link: ""
+          },
+          {
+            title: "Options",
+            link: "",
+            sub_items: [
+              {
+                title: "CBA",
+                link: ""
+              },
+              {
+                title: "Functions Option",
+                link: ""
+              },
+              {
+                title: "Import-Export",
+                link: ""
               },
 
             ]
@@ -312,129 +263,40 @@ const templateData =
         title: "Tutorial",
         link: "#",
         sub_items: [
-                    {
-                      title: "Registration Demo",
-                      link:"",
-                      sub_items:[]
-                    },
-                    {
-                      title: "Facebook Demo",
-                      link:"",
-                      sub_items:[]
-                    },
-                    {
-                      title: "Recording",
-                      link:"",
-                      sub_items:[]
-                    },
-                    {
-                      title: "Video Presentation for CBA",
-                      link:"",
-                      sub_items:[]
-                    }
-        ]
-      },
-      {
-        title: "Ready Projects",
-        link:"",
-        sub_items:[
-                  {
-                    title: "FB like/unlike",
-                    link:"",
-                    sub_items:[]
-                  }
-                ]
-
           {
             title: "Registration Demo",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Facebook Demo",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Recording",
-            link: "",
-            sub_items: []
+            link: ""
           },
           {
             title: "Video Presentation for CBA",
-            link: "",
-            sub_items: []
+            link: ""
           }
         ]
-
       },
-
       {
-
-        title: "What's New",
-        link:"",
-        sub_items:[
-                  {
-                    title: "Version 8.0",
-                    link:"",
-                    sub_items:[]
-                  },
-                  {
-                    title: "Version 6.0",
-                    link:"",
-                    sub_items:[]
-                  },
-                  {
-                    title: "Version 5.0",
-                    link:"",
-                    sub_items:[]
-                  }
-        ]
-      }
-
         title: "Ready Projects",
         link: "",
         sub_items: [
           {
             title: "FB like/unlike",
-            link: "",
-            sub_items: []
-          }
-        ]
-      },
-
-      {
-        title: "What's New",
-        link: "",
-        sub_items: [
-          {
-            title: "Version 8.0",
-            link: "",
-            sub_items: []
-          },
-          {
-            title: "Version 6.0",
-            link: "",
-            sub_items: []
-          },
-          {
-            title: "Version 5.0",
-            link: "",
-            sub_items: []
+            link: ""
           }
         ]
       }
+    ]
+  }
 };
 
 
-
-
-  const port = {
-  http: 3000};
-module.exports = {templateData,port};
 const port = {
   http: 3000
 };
 module.exports = { templateData, port };
-
-
