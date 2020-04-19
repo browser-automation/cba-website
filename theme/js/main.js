0a9f7d70d3a94a520c677c5428ee4d9c08b9525f
naarrek <h.narek@gmail.com>
2020-04-19 18:43:58 +0400

    several changes

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,3 +1,11 @@
 const {message} = require("./_module");
 
 console.log(message);
+
+// Mobile Menu
+const mobMenu = document.querySelector("#mobile-nav");
+mobMenu.addEventListener("change", e =>
+{
+  const data = document.querySelector(".data");
+  data.textContent = `Data to be added here.`;
+});
