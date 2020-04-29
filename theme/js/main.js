011464fc146a884bf2317124f63dc113a7f37b33
naarrek <h.narek@gmail.com>
2020-04-29 16:50:42 +0400

    Issue #5 - Implemented header with drop down navigation menu

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,5 +1,5 @@
 // Mobile Menu
-const mobMenu = document.querySelector("#mobile-nav");
+const mobMenu = document.querySelector("#mobile-nav select");
 mobMenu.addEventListener("change", e =>
 {
   window.location.pathname = e.target.selectedOptions[0].value;
